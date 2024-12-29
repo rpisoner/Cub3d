08fb1a307c684ff604b2de3826494f03a17e2806
jolivare <jolivare@student.42madrid.com>
2024-12-29 13:06:35 +0100

    add correct direction of header in parse config2.c

--- src/parser/parse_config2.c
+++ src/parser/parse_config2.c
@@ -3,14 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   parse_config2.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jolivare <jolivare@student.42mad.com>      +#+  +:+       +#+        */
+/*   By: jolivare <jolivare@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/24 12:27:36 by rpisoner          #+#    #+#             */
-/*   Updated: 2024/12/20 14:30:40 by jolivare         ###   ########.fr       */
+/*   Updated: 2024/12/29 13:05:59 by jolivare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../inc/cub3d.h"
+#include "../../inc/cub3d.h"
 
 void	load_floor_color(t_game *game, int i)
 {
